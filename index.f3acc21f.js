08e58867f867048ecfe63c3784ffa3d79cf49062
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2023-12-16 10:33:27 +0000

    Deploying to gh-pages from @ Oleksii03/Dashboard@6109289eebad028c89d9f63f0ea2427d4e2e7259 🚀

---
+++ index.f3acc21f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-mob-menu");document.querySelector(".js-mob-menu-btn").addEventListener("click",(function(n){e.classList.toggle("is-visible")}))}();
+//# sourceMappingURL=index.f3acc21f.js.map
