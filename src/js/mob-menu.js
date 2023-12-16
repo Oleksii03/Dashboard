26e87b3cd38591b45e4649eed16c7d9155d1f919
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-12-16 17:53:23 +0200

    add-scroll

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,8 +1,12 @@
 const mobMenu = document.querySelector('.js-mob-menu');
 const mobMenuBtn = document.querySelector('.js-mob-menu-btn');
+const mobMenuBtnActive = document.querySelector('.js-mob-menu-icon');
+
+console.log(mobMenuBtnActive);
 
 mobMenuBtn.addEventListener('click', onMenuToggle);
 
 function onMenuToggle (e) {
   mobMenu.classList.toggle('is-visible');
+  mobMenuBtnActive.classList.toggle('active-menu__btn');
 }
