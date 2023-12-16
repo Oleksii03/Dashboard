8cf4a7badc64cade9b7a534b2d0355b4eca153a1
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2023-12-16 11:24:58 +0000

    Deploying to gh-pages from @ Oleksii03/Dashboard@4ed001741ffe8f68166043b822e0876eb314e852 🚀

--- index.df5f9a01.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".js-mob-menu");document.querySelector(".js-mob-menu-btn").addEventListener("click",(function(t){e.classList.toggle("is-visible")}));
-//# sourceMappingURL=index.df5f9a01.js.map
